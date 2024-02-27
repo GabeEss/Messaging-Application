1d55a99acd31382c331d2e45e178bbd5cd529703
Gabe S <gabesliwowicz@gmail.com>
2024-02-27 18:58:04 -0500

    Change redirect uri.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Auth0Provider
       domain={import.meta.env.VITE_AUTH_DOMAIN}
       clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
-      redirectUri={window.location.origin}
+      redirectUri={`https://gabe-messaging-application.netlify.app/`}
       audience={import.meta.env.VITE_AUTH_AUDIENCE}
       scope="openid profile email"
     >
